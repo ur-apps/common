328c6efcdc42bac11881f52848957503fa0e3a43
Garry Freeman <garryfreeman09@gmail.com>
2025-01-23 00:27:40 +0300

    feat: added common package with initial configuration and Prettier settings

---
+++ packages/common/prettier.config.mjs
@@ -0,0 +1,18 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  quoteProps: 'preserve',
+  trailingComma: 'es5',
+  arrowParens: 'always',
+};
+
+export default config;
