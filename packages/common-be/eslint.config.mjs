b0ad5e59828cc51801072934ec36af58031efd73
Garry Freeman <garryfreeman09@gmail.com>
2025-02-02 02:32:33 +0300

    feat(be): updated ESLint config

--- packages/common-be/eslint.config.mjs
+++ packages/common-be/eslint.config.mjs
@@ -1,12 +1,8 @@
-import { coreConfig } from '@ur-apps/common/eslint';
+import { baseConfigs } from '@ur-apps/common/eslint';
 import jseslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
-export default tseslint.config(
-  { files: ['**/*.{js,mjs,cjs,ts}'] },
-  { languageOptions: { globals: globals.node } },
-  jseslint.configs.recommended,
-  tseslint.configs.recommended,
-  coreConfig
-);
+export default tseslint.config(jseslint.configs.recommended, tseslint.configs.recommended, baseConfigs, {
+  languageOptions: { globals: globals.node },
+});
