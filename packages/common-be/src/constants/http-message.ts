c7731abc891795fb5bf979727d0b59f96769166f
Garry Freeman <garryfreeman09@gmail.com>
2025-05-10 13:39:31 +0300

    feat(common-be): added HTTP message enum for standardized error responses

---
+++ packages/common-be/src/constants/http-message.ts
@@ -0,0 +1,13 @@
+export enum HttpMessage {
+  BAD_REQUEST = 'Bad request',
+  INVALID = 'Validation error',
+  UNAUTHORIZED = 'Unauthorized',
+  FORBIDDEN = 'Forbidden',
+  NOT_FOUND = 'Not found',
+  CONFLICT = 'Conflict',
+  INTERNAL_SERVER_ERROR = 'Internal server error',
+  NOT_IMPLEMENTED = 'Not implemented',
+  BAD_GATEWAY = 'Bad gateway',
+  SERVICE_UNAVAILABLE = 'Service unavailable',
+  GATEWAY_TIMEOUT = 'Gateway timeout',
+}
