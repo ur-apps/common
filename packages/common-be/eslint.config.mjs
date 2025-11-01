ceaaa3b9f0919ca0d2d336b596b969a6b8966bd0
Garry Freeman <garryfreeman09@gmail.com>
2025-11-02 01:19:53 +0300

    fix(common-be): enhance ESLint config with parser options

--- packages/common-be/eslint.config.mjs
+++ packages/common-be/eslint.config.mjs
@@ -4,5 +4,10 @@ import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(jseslint.configs.recommended, tseslint.configs.recommended, baseConfigs, {
-  languageOptions: { globals: globals.node },
+  languageOptions: {
+    globals: globals.node,
+    parserOptions: {
+      tsconfigRootDir: import.meta.url,
+    },
+  },
 });
